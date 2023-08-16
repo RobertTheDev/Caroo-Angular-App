db4a59195d41695b3658dd1963ff8b3cb7dd318d
RobertTheDev <robertthedev@gmail.com>
2023-08-16 11:47:29 +0100

    feat: defined car colours

---
+++ api/lib/constants/carColours.ts
@@ -0,0 +1,21 @@
+// Car colours is an array of the acceptable colours as car in the database can be.
+
+const carColours = [
+  'beige',
+  'black',
+  'blue',
+  'brown',
+  'cream',
+  'gold',
+  'green',
+  'grey',
+  'orange',
+  'pink',
+  'purple',
+  'red',
+  'silver',
+  'white',
+  'yellow',
+];
+
+export default carColours;
