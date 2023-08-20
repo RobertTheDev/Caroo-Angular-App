97a61f04d96a1a2a2c86150ad1f2da543c69a2cc
RobertTheDev <robertthedev@gmail.com>
2023-08-20 21:58:36 +0100

    feat: exported sign up form component

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -3,9 +3,11 @@ import { CommonModule } from '@angular/common';
 import { SignUpFormComponent } from './sign-up-form/sign-up-form.component';
 import { LoginFormComponent } from './login-form/login-form.component';
 import { ProfileComponent } from './profile/profile.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [SignUpFormComponent, LoginFormComponent, ProfileComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
+  exports: [SignUpFormComponent],
 })
 export class AuthModule {}
