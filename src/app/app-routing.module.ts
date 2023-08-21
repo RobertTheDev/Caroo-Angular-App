66221c6c107fd9087b9715a17226ae35a4bf88d0
RobertTheDev <robertthedev@gmail.com>
2023-08-21 14:10:07 +0100

    feat: defined app routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CarsListComponent } from './car/cars-list/cars-list.component';
+import { CarDetailComponent } from './car/car-detail/car-detail.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: CarsListComponent },
+  { path: 'cars', component: CarsListComponent },
+  { path: 'cars/:id', component: CarDetailComponent },
+];
 
 @NgModule({
   imports: [
