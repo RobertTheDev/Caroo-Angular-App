716f118d402501ee55ce4dbda78b70a18aa09154
RobertTheDev <robertthedev@gmail.com>
2023-08-21 14:10:47 +0100

    feat: added car module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AuthModule } from './auth/auth.module';
 import { HttpClientModule } from '@angular/common/http';
+import { CarModule } from './car/car.module';
 
 @NgModule({
   declarations: [AppComponent],
@@ -13,6 +14,7 @@ import { HttpClientModule } from '@angular/common/http';
     AppRoutingModule,
     HttpClientModule,
     AuthModule,
+    CarModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
