2602142584076b622b5ba5110d81f1219cd0b8b7
RobertTheDev <robertthedev@gmail.com>
2023-08-21 14:11:07 +0100

    feat: created car service

--- src/app/services/car/car.service.ts
+++ src/app/services/car/car.service.ts
@@ -1,6 +1,21 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Car } from '@prisma/client';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
-export class CarService {}
+export class CarService {
+  private apiUrl = 'api/cars';
+
+  constructor(private http: HttpClient) {}
+
+  getCars(): Observable<{ data: Car[] }> {
+    return this.http.get<{ data: Car[] }>(this.apiUrl);
+  }
+
+  getCarById(): Observable<{ data: Car | null }> {
+    return this.http.get<{ data: Car | null }>(this.apiUrl);
+  }
+}
