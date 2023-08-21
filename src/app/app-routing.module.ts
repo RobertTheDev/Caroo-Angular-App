f14dfd4e483a10883a5b8272ff86ff84bdb19a3f
RobertTheDev <robertthedev@gmail.com>
2023-08-21 20:01:30 +0100

    feat: added info page routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,22 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CarsListComponent } from './car/cars-list/cars-list.component';
 import { CarDetailComponent } from './car/car-detail/car-detail.component';
+import { PrivacyPolicyComponent } from './info/privacy-policy/privacy-policy.component';
+import { TermsAndConditionsComponent } from './info/terms-and-conditions/terms-and-conditions.component';
+import { AccessibilityStatementComponent } from './info/accessibility-statement/accessibility-statement.component';
+import { AboutComponent } from './info/about/about.component';
 
 const routes: Routes = [
   { path: '', component: CarsListComponent },
   { path: 'cars', component: CarsListComponent },
   { path: 'cars/:id', component: CarDetailComponent },
+  { path: 'about', component: AboutComponent },
+  {
+    path: 'accessibility-statement',
+    component: AccessibilityStatementComponent,
+  },
+  { path: 'privacy-policy', component: PrivacyPolicyComponent },
+  { path: 'terms-and-conditions', component: TermsAndConditionsComponent },
 ];
 
 @NgModule({
