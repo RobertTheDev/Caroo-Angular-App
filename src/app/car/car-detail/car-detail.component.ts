5ef753185ec5f05c7ce56b0572a5307c3157cc7a
RobertTheDev <robertthedev@gmail.com>
2023-08-21 20:11:48 +0100

    feat: got car by id by route and rendered content

--- src/app/car/car-detail/car-detail.component.ts
+++ src/app/car/car-detail/car-detail.component.ts
@@ -1,8 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Car } from '@prisma/client';
+import { CarService } from 'src/app/services/car/car.service';
 
 @Component({
   selector: 'app-car-detail',
   templateUrl: './car-detail.component.html',
   styleUrls: ['./car-detail.component.css'],
 })
-export class CarDetailComponent {}
+export class CarDetailComponent implements OnInit {
+  car: Car | null = null;
+
+  constructor(
+    private route: ActivatedRoute,
+    private carService: CarService,
+  ) {}
+
+  ngOnInit() {
+    const id = this.route.snapshot.params['id'];
+
+    this.carService.getCarById(id).subscribe((data) => {
+      this.car = data.data;
+    });
+  }
+}
