93626521aff7a50b2521f5b5ba83c0ae5819661a
RobertTheDev <robertthedev@gmail.com>
2023-08-21 13:00:58 +0100

    feat: added user service

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -4,10 +4,12 @@ import { SignUpFormComponent } from './sign-up-form/sign-up-form.component';
 import { LoginFormComponent } from './login-form/login-form.component';
 import { ProfileComponent } from './profile/profile.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { UserService } from '../services/user/user.service';
 
 @NgModule({
   declarations: [SignUpFormComponent, LoginFormComponent, ProfileComponent],
   imports: [CommonModule, ReactiveFormsModule],
-  exports: [SignUpFormComponent],
+  exports: [SignUpFormComponent, ProfileComponent],
+  providers: [UserService],
 })
 export class AuthModule {}
