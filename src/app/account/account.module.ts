8051109fc655b33b36186de5ea06034d84fba415
RobertTheDev <robertthedev@gmail.com>
2023-08-22 09:33:48 +0100

    feat: created account module

--- src/app/account/account.module.ts
+++ src/app/account/account.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { AccountDetailComponent } from './account-detail/account-detail.component';
+import { ChangePasswordFormComponent } from './change-password-form/change-password-form.component';
+import { ChangeEmailFormComponent } from './change-email-form/change-email-form.component';
+import { CloseAccountFormComponent } from './close-account-form/close-account-form.component';
+import { AccountSettingsMenuComponent } from './account-settings-menu/account-settings-menu.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    AccountDetailComponent,
+    ChangePasswordFormComponent,
+    ChangeEmailFormComponent,
+    CloseAccountFormComponent,
+    AccountSettingsMenuComponent
+  ],
   imports: [
     CommonModule
   ]
