3cbed4126f1939759ac75c39bdbd2aa0ee03fa73
RobertTheDev <robertthedev@gmail.com>
2023-08-22 09:33:16 +0100

    feat: created change email component

---
+++ src/app/account/change-email-form/change-email-form.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-change-email-form',
+  templateUrl: './change-email-form.component.html',
+  styleUrls: ['./change-email-form.component.css']
+})
+export class ChangeEmailFormComponent {
+
+}
