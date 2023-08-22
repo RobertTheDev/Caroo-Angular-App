3ff08ef08ed4473e07fae56029f8abe99b36ce4e
RobertTheDev <robertthedev@gmail.com>
2023-08-22 09:35:06 +0100

    feat: created car module

--- src/app/car/car.module.ts
+++ src/app/car/car.module.ts
@@ -4,9 +4,20 @@ import { CarsListComponent } from './cars-list/cars-list.component';
 import { CarDetailComponent } from './car-detail/car-detail.component';
 import { CarService } from '../services/car/car.service';
 import { RouterModule } from '@angular/router';
+import { CarFilterMenuComponent } from './car-filter-menu/car-filter-menu.component';
+import { CarFilterListComponent } from './car-filter-list/car-filter-list.component';
+import { CarCreateFormComponent } from './car-create-form/car-create-form.component';
+import { CarEditFormComponent } from './car-edit-form/car-edit-form.component';
 
 @NgModule({
-  declarations: [CarsListComponent, CarDetailComponent],
+  declarations: [
+    CarsListComponent,
+    CarDetailComponent,
+    CarFilterMenuComponent,
+    CarFilterListComponent,
+    CarCreateFormComponent,
+    CarEditFormComponent,
+  ],
   imports: [CommonModule, RouterModule],
   exports: [CarsListComponent, CarDetailComponent],
   providers: [CarService],
