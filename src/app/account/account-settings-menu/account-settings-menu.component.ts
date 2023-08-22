a96fe965f72fd1f42726f7498f7061a94ed59719
RobertTheDev <robertthedev@gmail.com>
2023-08-22 11:14:34 +0100

    feat: mapped account settings menu links

--- src/app/account/account-settings-menu/account-settings-menu.component.ts
+++ src/app/account/account-settings-menu/account-settings-menu.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import accountSettingsLinks from 'src/lib/links/accountSettingsLinks';
 
 @Component({
   selector: 'app-account-settings-menu',
   templateUrl: './account-settings-menu.component.html',
-  styleUrls: ['./account-settings-menu.component.css']
+  styleUrls: ['./account-settings-menu.component.css'],
 })
 export class AccountSettingsMenuComponent {
-
+  accountSettingsLinks = accountSettingsLinks;
 }
