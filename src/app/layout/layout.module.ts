27452924884c0fb17412a9dbec1db8cdfe8160f1
RobertTheDev <robertthedev@gmail.com>
2023-08-22 09:35:28 +0100

    feat: created layout module

--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { RouterModule } from '@angular/router';
+import { PageLayoutComponent } from './page-layout/page-layout.component';
 
 @NgModule({
-  declarations: [HeaderComponent, FooterComponent],
+  declarations: [HeaderComponent, FooterComponent, PageLayoutComponent],
   imports: [CommonModule, RouterModule],
   exports: [FooterComponent, HeaderComponent],
 })
