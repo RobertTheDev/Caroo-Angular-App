7324df1ee4d2b8abbc8f62fc7e10fa103025cca6
RobertTheDev <robertthedev@gmail.com>
2023-08-22 09:33:04 +0100

    feat: created change password form component

---
+++ src/app/account/change-password-form/change-password-form.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-change-password-form',
+  templateUrl: './change-password-form.component.html',
+  styleUrls: ['./change-password-form.component.css']
+})
+export class ChangePasswordFormComponent {
+
+}
