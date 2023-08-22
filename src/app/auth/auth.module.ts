700f312333a2147ef31e98348f2d1122865561b9
RobertTheDev <robertthedev@gmail.com>
2023-08-22 09:34:10 +0100

    feat: created auth module

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -2,14 +2,18 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SignUpFormComponent } from './sign-up-form/sign-up-form.component';
 import { LoginFormComponent } from './login-form/login-form.component';
-import { ProfileComponent } from './profile/profile.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { UserService } from '../services/user/user.service';
+import { ForgotPasswordFormComponent } from './forgot-password-form/forgot-password-form.component';
 
 @NgModule({
-  declarations: [SignUpFormComponent, LoginFormComponent, ProfileComponent],
+  declarations: [
+    SignUpFormComponent,
+    LoginFormComponent,
+    ForgotPasswordFormComponent,
+  ],
   imports: [CommonModule, ReactiveFormsModule],
-  exports: [SignUpFormComponent, ProfileComponent],
+  exports: [SignUpFormComponent],
   providers: [UserService],
 })
 export class AuthModule {}
