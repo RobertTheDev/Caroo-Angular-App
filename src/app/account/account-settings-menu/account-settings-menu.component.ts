ce5f9e8d1f8077bbd4455cab7d658eb855147601
RobertTheDev <robertthedev@gmail.com>
2023-08-22 09:32:14 +0100

    feat: created account settings menu component

---
+++ src/app/account/account-settings-menu/account-settings-menu.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-account-settings-menu',
+  templateUrl: './account-settings-menu.component.html',
+  styleUrls: ['./account-settings-menu.component.css']
+})
+export class AccountSettingsMenuComponent {
+
+}
