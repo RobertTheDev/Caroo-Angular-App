d71baf98a53c5c3b49e174be4b5ba6edeef7117f
RobertTheDev <robertthedev@gmail.com>
2023-08-22 09:35:22 +0100

    feat: created saved car module

--- src/app/saved-car/saved-car.module.ts
+++ src/app/saved-car/saved-car.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { SavedCarsListComponent } from './saved-cars-list/saved-cars-list.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    SavedCarsListComponent
+  ],
   imports: [
     CommonModule
   ]
