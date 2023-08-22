046846df86ca7945b2d158a609ac9ef1fa7d4e22
RobertTheDev <robertthedev@gmail.com>
2023-08-22 09:29:59 +0100

    feat: created car owner component

---
+++ src/app/car-owner/car-owner-list/car-owner-list.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-car-owner-list',
+  templateUrl: './car-owner-list.component.html',
+  styleUrls: ['./car-owner-list.component.css']
+})
+export class CarOwnerListComponent {
+
+}
