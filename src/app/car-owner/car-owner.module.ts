129dc7d016e467e56f92be0daff3c81a844141ae
RobertTheDev <robertthedev@gmail.com>
2023-08-22 09:35:16 +0100

    feat: created car owner module

---
+++ src/app/car-owner/car-owner.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CarOwnerListComponent } from './car-owner-list/car-owner-list.component';
+
+
+
+@NgModule({
+  declarations: [
+    CarOwnerListComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class CarOwnerModule { }
