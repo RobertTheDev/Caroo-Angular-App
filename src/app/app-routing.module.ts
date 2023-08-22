0e8fafc742900a97de2ea37a857b6cac3a805335
RobertTheDev <robertthedev@gmail.com>
2023-08-22 11:14:54 +0100

    feat: added profile links

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,11 +6,23 @@ import { PrivacyPolicyComponent } from './info/privacy-policy/privacy-policy.com
 import { TermsAndConditionsComponent } from './info/terms-and-conditions/terms-and-conditions.component';
 import { AccessibilityStatementComponent } from './info/accessibility-statement/accessibility-statement.component';
 import { AboutComponent } from './info/about/about.component';
+import { AccountDetailComponent } from './account/account-detail/account-detail.component';
+import { CarOwnerListComponent } from './car-owner/car-owner-list/car-owner-list.component';
+import { ChangeEmailFormComponent } from './account/change-email-form/change-email-form.component';
+import { ChangePasswordFormComponent } from './account/change-password-form/change-password-form.component';
+import { CloseAccountFormComponent } from './account/close-account-form/close-account-form.component';
+import { AccountEditProfileComponent } from './account/account-edit-profile/account-edit-profile.component';
 
 const routes: Routes = [
   { path: '', component: CarsListComponent },
   { path: 'cars', component: CarsListComponent },
   { path: 'cars/:id', component: CarDetailComponent },
+  { path: 'profile', component: AccountDetailComponent },
+  { path: 'profile/close-account', component: CloseAccountFormComponent },
+  { path: 'profile/change-password', component: ChangePasswordFormComponent },
+  { path: 'profile/change-email', component: ChangeEmailFormComponent },
+  { path: 'profile/edit-profile', component: AccountEditProfileComponent },
+  { path: 'profile/my-car-listings', component: CarOwnerListComponent },
   { path: 'about', component: AboutComponent },
   {
     path: 'accessibility-statement',
