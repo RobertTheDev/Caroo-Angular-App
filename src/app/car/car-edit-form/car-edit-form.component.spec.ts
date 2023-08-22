157c9aba083e40fe5cf070bb67e65277f9d7cdb5
RobertTheDev <robertthedev@gmail.com>
2023-08-22 09:31:36 +0100

    feat: created car edit form component

---
+++ src/app/car/car-edit-form/car-edit-form.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { CarEditFormComponent } from './car-edit-form.component';
+
+describe('CarEditFormComponent', () => {
+  let component: CarEditFormComponent;
+  let fixture: ComponentFixture<CarEditFormComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ CarEditFormComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(CarEditFormComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/car/car-edit-form/car-edit-form.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-car-edit-form',
+  templateUrl: './car-edit-form.component.html',
+  styleUrls: ['./car-edit-form.component.css']
+})
+export class CarEditFormComponent {
+
+}
