9715930eb6979af5603ac58de0e17a6004feb7da
RobertTheDev <robertthedev@gmail.com>
2023-08-23 15:44:34 +0100

    feat: added auth navigation buttons

--- src/app/modules/layout/components/header/header.component.ts
+++ src/app/modules/layout/components/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 import companyName from 'src/app/lib/constants/companyName';
 import headerLinks from 'src/app/lib/links/headerLinks';
 
@@ -10,4 +11,10 @@ import headerLinks from 'src/app/lib/links/headerLinks';
 export class HeaderComponent {
   headerLinks = headerLinks;
   companyName = companyName;
+
+  constructor(private router: Router) {}
+
+  navigateTo(url: string) {
+    this.router.navigateByUrl(url);
+  }
 }
