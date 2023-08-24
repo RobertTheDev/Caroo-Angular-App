217776811e502396712d42c2a1dca4a1784204a9
RobertTheDev <robertthedev@gmail.com>
2023-08-25 00:35:57 +0100

    feat: added seo to not found view

--- src/app/modules/not-found/views/not-found-view/not-found-view.component.ts
+++ src/app/modules/not-found/views/not-found-view/not-found-view.component.ts
@@ -1,8 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Meta, Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-not-found-view',
   templateUrl: './not-found-view.component.html',
   styleUrls: ['./not-found-view.component.css'],
 })
-export class NotFoundViewComponent {}
+export class NotFoundViewComponent implements OnInit {
+  constructor(
+    private meta: Meta,
+    private title: Title,
+  ) {}
+
+  ngOnInit(): void {
+    this.title.setTitle('Not Found | Caroo');
+
+    this.meta.updateTag({
+      name: 'description',
+      content:
+        "Lost in the journey? Unfortunately, the page you sought isn't here. Discover Caroo's world of vehicles and connections as you explore our platform.",
+    });
+  }
+}
