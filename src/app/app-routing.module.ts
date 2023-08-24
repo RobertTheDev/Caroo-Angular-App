9af59d280bb1255aa770020405c6a1069307dd4d
RobertTheDev <robertthedev@gmail.com>
2023-08-24 18:46:56 +0100

    feat: added scroll restoration

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -59,6 +59,7 @@ const routes: Routes = [
   imports: [
     RouterModule.forRoot(routes, {
       initialNavigation: 'enabledBlocking',
+      scrollPositionRestoration: 'enabled',
     }),
   ],
   exports: [RouterModule],
