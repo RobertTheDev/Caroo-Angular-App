3d3e90d284a084dee20b5acd609b4607c9e23495
RobertTheDev <robertthedev@gmail.com>
2023-08-24 12:07:08 +0100

    feat: created burger menu component

--- src/app/modules/layout/layout.module.ts
+++ src/app/modules/layout/layout.module.ts
@@ -5,9 +5,15 @@ import { FooterComponent } from './components/footer/footer.component';
 import { RouterModule } from '@angular/router';
 import { PageLayoutComponent } from './components/page-layout/page-layout.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { BurgerMenuComponent } from './components/burger-menu/burger-menu.component';
 
 @NgModule({
-  declarations: [HeaderComponent, FooterComponent, PageLayoutComponent],
+  declarations: [
+    HeaderComponent,
+    FooterComponent,
+    PageLayoutComponent,
+    BurgerMenuComponent,
+  ],
   imports: [CommonModule, RouterModule, FontAwesomeModule],
   exports: [PageLayoutComponent],
 })
