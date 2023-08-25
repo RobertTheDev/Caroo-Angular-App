06473cfb88e993d8c66aa6f1642d25cc24384b2c
RobertTheDev <robertthedev@gmail.com>
2023-08-25 22:29:49 +0100

    ci: prettier formatted file

--- src/app/modules/car/views/car-view/car-view.component.ts
+++ src/app/modules/car/views/car-view/car-view.component.ts
@@ -3,8 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-car-view',
   templateUrl: './car-view.component.html',
-  styleUrls: ['./car-view.component.css']
+  styleUrls: ['./car-view.component.css'],
 })
-export class CarViewComponent {
-
-}
+export class CarViewComponent {}
