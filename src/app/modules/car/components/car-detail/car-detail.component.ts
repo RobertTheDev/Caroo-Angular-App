aa47378b16270e0a470e72987680f4e61db6069a
RobertTheDev <robertthedev@gmail.com>
2023-08-25 22:30:03 +0100

    feat: added seo title and description

--- src/app/modules/car/components/car-detail/car-detail.component.ts
+++ src/app/modules/car/components/car-detail/car-detail.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Meta, Title } from '@angular/platform-browser';
 import { ActivatedRoute } from '@angular/router';
 import { CarService } from 'src/app/services/car/car.service';
 import CarWithImage from 'src/app/types/CarWithImages';
@@ -14,6 +15,8 @@ export class CarDetailComponent implements OnInit {
   errorMessage: string | null = null;
 
   constructor(
+    private meta: Meta,
+    private title: Title,
     private route: ActivatedRoute,
     private carService: CarService,
   ) {}
@@ -25,6 +28,23 @@ export class CarDetailComponent implements OnInit {
       next: (data) => {
         this.loading = false;
         this.car = data.data;
+
+        if (this.car) {
+          const { year, make, model } = this.car;
+
+          this.title.setTitle(`${year} ${make} ${model} | Caroo`);
+
+          this.meta.updateTag({
+            name: 'description',
+            content: `Buy a used ${make} ${model} from caroo. Explore a wide selection of high-quality, second-hand used cars for sale in your local area at Caroo. Find the perfect vehicle to meet your needs and budget. Start your car search with confidence at Caroo.`,
+          });
+        } else {
+          this.title.setTitle(`Car | Caroo`);
+          this.meta.updateTag({
+            name: 'description',
+            content: `Explore a wide selection of high-quality, second-hand used cars for sale in your local area at Caroo. Find the perfect vehicle to meet your needs and budget. Start your car search with confidence at Caroo.`,
+          });
+        }
       },
       error: (error) => {
         this.loading = false;
