6c40a378739b0e496ff66795ccf4e3ccd00285d1
RobertTheDev <robertthedev@gmail.com>
2023-08-25 04:57:11 +0100

    feat: added style and seo to auth forms

--- src/app/modules/auth/views/forgot-password-view/forgot-password-view.component.ts
+++ src/app/modules/auth/views/forgot-password-view/forgot-password-view.component.ts
@@ -1,8 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Meta, Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-forgot-password-view',
   templateUrl: './forgot-password-view.component.html',
   styleUrls: ['./forgot-password-view.component.css'],
 })
-export class ForgotPasswordViewComponent {}
+export class ForgotPasswordViewComponent implements OnInit {
+  constructor(
+    private meta: Meta,
+    private title: Title,
+  ) {}
+  ngOnInit(): void {
+    this.title.setTitle('Forgot Password | Caroo');
+
+    this.meta.updateTag({
+      name: 'description',
+      content:
+        'If you have forgotten your password - you can receive an email for a new password request by entering and submitting your email address below.',
+    });
+  }
+}
--- src/app/modules/auth/views/login-view/login-view.component.ts
+++ src/app/modules/auth/views/login-view/login-view.component.ts
@@ -1,8 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Meta, Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-login-view',
   templateUrl: './login-view.component.html',
   styleUrls: ['./login-view.component.css'],
 })
-export class LoginViewComponent {}
+export class LoginViewComponent implements OnInit {
+  constructor(
+    private meta: Meta,
+    private title: Title,
+  ) {}
+  ngOnInit(): void {
+    this.title.setTitle('Login | Caroo');
+
+    this.meta.updateTag({
+      name: 'description',
+      content:
+        'Login to your account by entering and submitting your email address and password below. If you do not have an account you can sign up on Caroo by clicking the sign up link.',
+    });
+  }
+}
