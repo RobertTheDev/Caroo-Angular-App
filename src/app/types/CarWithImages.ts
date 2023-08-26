102b2fefd92e782964cbc1903ce3444ede704438
RobertTheDev <robertthedev@gmail.com>
2023-08-26 01:31:41 +0100

    ci: added user type

--- src/app/types/CarWithImages.ts
+++ src/app/types/CarWithImages.ts
@@ -2,4 +2,11 @@ import { Car, CarImage } from '@prisma/client';
 
 export default interface CarWithImage extends Car {
   images: CarImage[];
+  owner: {
+    id: string;
+    email: string;
+    firstName: string;
+    lastName: string;
+    avatarUrl: string;
+  };
 }
