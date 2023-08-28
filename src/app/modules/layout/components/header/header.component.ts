3593838db19e996a96f36def1f888cf71edc488c
RobertTheDev <robertthedev@gmail.com>
2023-08-28 14:26:33 +0100

    feat: added header controls

--- src/app/modules/layout/components/header/header.component.ts
+++ src/app/modules/layout/components/header/header.component.ts
@@ -1,6 +1,10 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
-import { faHeart, faUser } from '@fortawesome/free-regular-svg-icons';
+import {
+  faHeart,
+  faMessage,
+  faUser,
+} from '@fortawesome/free-regular-svg-icons';
 import { faBars } from '@fortawesome/free-solid-svg-icons';
 import companyName from 'src/app/lib/constants/companyName';
 import headerLinks from 'src/app/lib/links/headerLinks';
@@ -16,10 +20,41 @@ export class HeaderComponent {
   faBars = faBars;
   faHeart = faHeart;
   faUser = faUser;
+  faMessage = faMessage;
+  avatarUrl =
+    'https://lh3.googleusercontent.com/a/AAcHTtc7om1QDN6eGEuyZGQ4OxXiq8sehpW5JXbhhj0QRZSMBg=s576-c-no';
+  controlMouseOverActiveName: string | null = null;
+  userSignedIn = false;
 
   constructor(private router: Router) {}
+
+  handleControlMouseOver(name: string) {
+    this.controlMouseOverActiveName = name;
+  }
+
+  handleControlMouseOut() {
+    this.controlMouseOverActiveName = null;
+  }
 
   navigateTo(url: string) {
     this.router.navigateByUrl(url);
   }
+
+  headerControls = [
+    {
+      icon: faHeart,
+      name: 'Saved',
+      url: '/saved-cars',
+    },
+    {
+      icon: faMessage,
+      name: 'Messages',
+      url: '/chat',
+    },
+    {
+      icon: faUser,
+      name: 'Profile',
+      url: '/profile',
+    },
+  ];
 }
