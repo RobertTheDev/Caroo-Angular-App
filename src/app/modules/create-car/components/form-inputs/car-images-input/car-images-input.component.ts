09c7336fa8e402aa0dc85f40a35c6558544ec0e3
RobertTheDev <robertthedev@gmail.com>
2023-08-29 21:50:24 +0100

    feat: moved car images input directory

--- src/app/modules/create-car/components/form-inputs/car-images-input/car-images-input.component.ts
+++ src/app/modules/create-car/components/form-inputs/car-images-input/car-images-input.component.ts
@@ -3,8 +3,32 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-car-images-input',
   templateUrl: './car-images-input.component.html',
-  styleUrls: ['./car-images-input.component.css']
+  styleUrls: ['./car-images-input.component.css'],
 })
 export class CarImagesInputComponent {
+  images: string[] = new Array(6).fill('');
 
+  replaceImage(index: number) {
+    // Handle replacing the image at the specified index
+    const newImageUrl = prompt('Enter image URL:');
+    if (newImageUrl) {
+      this.images[index] = newImageUrl;
+    }
+  }
+
+  deleteImage(index: number) {
+    // Handle deleting the image at the specified index
+    this.images[index] = '';
+  }
+
+  uploadImage(event: any, index: number) {
+    const file = event.target.files[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.onload = (e: any) => {
+        this.images[index] = e.target.result;
+      };
+      reader.readAsDataURL(file);
+    }
+  }
 }
