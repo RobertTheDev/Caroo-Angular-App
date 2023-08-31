f64166f97617f421aeafff48e658cc593fa278fe
RobertTheDev <robertthedev@gmail.com>
2023-08-31 01:27:41 +0100

    ci: deleted car owner list

--- src/app/modules/car-owner/components/car-owner-list/car-owner-list.component.ts
+++
@@ -1,7 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-car-owner-list',
-  templateUrl: './car-owner-list.component.html',
-})
-export class CarOwnerListComponent {}
