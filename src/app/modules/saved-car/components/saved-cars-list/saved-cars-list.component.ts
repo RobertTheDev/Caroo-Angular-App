21545748aba39b147996f8f84228830d5ad4c153
RobertTheDev <robertthedev@gmail.com>
2023-08-31 01:47:24 +0100

    ci: deleted css default file

--- src/app/modules/saved-car/components/saved-cars-list/saved-cars-list.component.ts
+++ src/app/modules/saved-car/components/saved-cars-list/saved-cars-list.component.ts
@@ -3,6 +3,5 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-saved-cars-list',
   templateUrl: './saved-cars-list.component.html',
-  styleUrls: ['./saved-cars-list.component.css'],
 })
 export class SavedCarsListComponent {}
