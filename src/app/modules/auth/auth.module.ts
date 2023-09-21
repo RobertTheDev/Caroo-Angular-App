317071418a2741bf7d967b01d1b5d94abcbe81e9
RobertTheDev <robertthedev@gmail.com>
2023-09-21 05:59:39 +0100

    ci: deleted forgot password component and page

--- src/app/modules/auth/auth.module.ts
+++ src/app/modules/auth/auth.module.ts
@@ -4,27 +4,20 @@ import { SignUpFormComponent } from './components/sign-up-form/sign-up-form.comp
 import { LoginFormComponent } from './components/login-form/login-form.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { UserService } from '../../services/user/user.service';
-import { ForgotPasswordFormComponent } from './components/forgot-password-form/forgot-password-form.component';
 import { SignUpViewComponent } from './views/sign-up-view/sign-up-view.component';
 import { LoginViewComponent } from './views/login-view/login-view.component';
-import { ForgotPasswordViewComponent } from './views/forgot-password-view/forgot-password-view.component';
 import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
     SignUpFormComponent,
     LoginFormComponent,
-    ForgotPasswordFormComponent,
+
     SignUpViewComponent,
     LoginViewComponent,
-    ForgotPasswordViewComponent,
   ],
   imports: [CommonModule, ReactiveFormsModule, RouterModule],
-  exports: [
-    SignUpViewComponent,
-    LoginViewComponent,
-    ForgotPasswordViewComponent,
-  ],
+  exports: [SignUpViewComponent, LoginViewComponent],
   providers: [UserService],
 })
 export class AuthModule {}
