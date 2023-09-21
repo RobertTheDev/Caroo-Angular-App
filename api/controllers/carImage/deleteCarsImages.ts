0b6d33b5048cd0ff799476ab1b421e0ecc804389
RobertTheDev <robertthedev@gmail.com>
2023-09-21 19:17:03 +0100

    feat: created delete car images handler

---
+++ api/controllers/carImage/deleteCarsImages.ts
@@ -0,0 +1,26 @@
+import CarImagePrismaService from 'api/providers/prisma/carImage.service';
+import { Request, Response } from 'express';
+import { ReasonPhrases, StatusCodes } from 'http-status-codes';
+
+// This controller gets a car image by its id.
+
+export default async function deleteCarImages(_req: Request, res: Response) {
+  try {
+    // Declare and use car image service.
+    const carImagePrismaService = new CarImagePrismaService();
+
+    // Delete all car images.
+    await carImagePrismaService.deleteAll();
+
+    // Return car image.
+    res.status(StatusCodes.OK).send({
+      message: `Successfully deleted car images.`,
+    });
+  } catch (error) {
+    // Catch and return an error if found.
+    res.status(StatusCodes.INTERNAL_SERVER_ERROR).send({
+      message: ReasonPhrases.INTERNAL_SERVER_ERROR,
+      error,
+    });
+  }
+}
