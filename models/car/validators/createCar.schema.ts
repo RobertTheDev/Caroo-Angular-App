866e0751075f753fd1a4e560991bd1049f42d8e2
RobertTheDev <robertthedev@gmail.com>
2023-09-21 02:57:37 +0100

    feat: defined car validators

--- models/car/validators/createCar.schema.ts
+++ models/car/validators/createCar.schema.ts
@@ -1,73 +1,114 @@
-import carColours from '../../../api/lib/constants/carColours';
-import { number, object, string } from 'zod';
+import { number, object, string, z } from 'zod';
+import carColourValues from '../values/carColourValues';
+import carGearboxValues from '../values/carGearboxValues';
+import carFuelTypeValues from '../values/carFuelTypeValues';
+import carDriveTypeValues from '../values/carDriveTypeValues';
+import carEngineSizeValues from '../values/carEngineSizeValues';
 
-// Create car schema is a validation schema defining the acceptable fields required.
-
+// Zod validation schema defines fields required for creating a car.
 const createCarSchema = object({
-  description: string({
-    required_error:
-      'A car description is required. Please provide a car description.',
-    invalid_type_error: 'Car description must be a string.',
-  }).optional(),
-  doors: number({
-    required_error: 'A car price is required. Please provide a car price.',
-    invalid_type_error: 'Car price must be a string.',
-  }),
-  engineSizeTotal: number({
-    required_error: 'A car price is required. Please provide a car price.',
-    invalid_type_error: 'Car price must be a string.',
-  }),
-  seats: number({
-    required_error: 'A car price is required. Please provide a car price.',
-    invalid_type_error: 'Car price must be a string.',
-  }),
-  mileageTotal: number({
-    required_error: 'A car price is required. Please provide a car price.',
-    invalid_type_error: 'Car price must be a string.',
-  }),
-  priceTotal: number({
-    required_error: 'A car price is required. Please provide a car price.',
-    invalid_type_error: 'Car price must be a string.',
-  }),
-  driveType: string({
-    required_error:
-      'A car drive type is required. Please provide a car drive type.',
-    invalid_type_error: 'Car drive type must be a string.',
-  }),
-  fuelType: string({
-    required_error:
-      'A car fuel type is required. Please provide a car fuel type.',
-    invalid_type_error: 'Car fuel type must be a string.',
-  }),
-  gearbox: string({
-    required_error: 'A car gearbox is required. Please provide a car gearbox.',
-    invalid_type_error: 'Car gearbox must be a string.',
-  }),
-  make: string({
-    required_error: 'A car make is required. Please provide a car make.',
-    invalid_type_error: 'Car make must be a string.',
-  }),
-  model: string({
-    required_error: 'A car model is required. Please provide a car model.',
-    invalid_type_error: 'Car model must be a string.',
-  }),
   colour: string({
     required_error:
-      'A colour for the car is required. Please provide a colour.',
-    invalid_type_error: 'Colour must be a string.',
-  }).refine((value) => carColours.includes(value), {
-    message: 'Colour must be one of "blue", "red", "orange", or "green".',
-  }),
-  year: number({
-    required_error: 'A year for the car is required. Please provide a year.',
-    invalid_type_error: 'Year must be a number.',
-  }).refine((value) => value >= 1950 && value <= new Date().getFullYear(), {
-    message: `Year must be a valid year between 1950 and ${new Date().getFullYear()}.`,
-  }),
+      'A car colour is required. Please provide a colour for the car you are selling.',
+    invalid_type_error: 'Car colour must be a string.',
+  })
+    .nonempty('Car colour cannot be empty.')
+    .refine((value) => carColourValues.includes(value), {
+      message: `Car colour must be one of ${carColourValues}.`,
+    }),
+  doors: number({
+    required_error:
+      'Number of card doors is required. Please provide a number of doors for the car you are selling.',
+    invalid_type_error: 'Car doors must be a string.',
+  })
+    .min(2, 'Car must have at least two doors.')
+    .max(5, 'Car must have at least five doors.'),
+  description: string({
+    required_error:
+      'A car description is required. Please provide a car description for the car you are selling.',
+    invalid_type_error: 'Car description must be a string.',
+  })
+    .nonempty('Car description cannot be empty.')
+    .optional(),
+  driveType: string({
+    required_error:
+      'A car drive type is required. Please provide a car drive type for the car you are selling.',
+    invalid_type_error: 'Car drive type must be a string.',
+  })
+    .nonempty('Car drive type cannot be empty.')
+    .refine((value) => carDriveTypeValues.includes(value), {
+      message: `Car drive type must be one of ${carDriveTypeValues}.`,
+    }),
+  engineSizeTotal: string({
+    required_error:
+      'A car engine size is required. Please provide a car engine size for the car you are selling.',
+    invalid_type_error: 'Car engine size must be a string.',
+  })
+    .nonempty('Car engine size cannot be empty.')
+    .refine((value) => carEngineSizeValues.includes(value), {
+      message: `Car engine size must be one of ${carEngineSizeValues}.`,
+    }),
+  fuelType: string({
+    required_error:
+      'A car fuel type is required. Please provide a car fuel type for the car you are selling.',
+    invalid_type_error: 'Car fuel type must be a string.',
+  })
+    .nonempty('Car fuel type cannot be empty.')
+    .refine((value) => carFuelTypeValues.includes(value), {
+      message: `Car fuel type must be one of ${carFuelTypeValues}.`,
+    }),
+  gearbox: string({
+    required_error:
+      'A car gearbox is required. Please provide a car gearbox for the car you are selling.',
+    invalid_type_error: 'Car gearbox must be a string.',
+  })
+    .nonempty('Car Gearbox cannot be empty.')
+    .refine((value) => carGearboxValues.includes(value), {
+      message: `Car gearbox must be one of ${carGearboxValues}.`,
+    }),
+  make: string({
+    required_error:
+      'A car make is required. Please provide a car make for the car you are selling.',
+    invalid_type_error: 'Car make must be a string.',
+  }).nonempty('Car make cannot be empty.'),
+
+  mileageTotal: number({
+    required_error:
+      'Car mileage is required. Please provide a mileage for the car you are selling.',
+    invalid_type_error: 'Car mileage must be a number.',
+  }).min(0, 'Mileage must be a positive number.'),
+  model: string({
+    required_error:
+      'A car model is required. Please provide a car model for the car you are selling.',
+    invalid_type_error: 'Car model must be a string.',
+  }).nonempty('Car model cannot be empty.'),
   ownerId: string({
-    required_error: 'An owner ID is required. Please provide an owner ID.',
-    invalid_type_error: 'Owner ID be a string.',
+    required_error:
+      'A car owner ID is required. Please provide an owner ID for the car you are selling.',
+    invalid_type_error: 'Car owner ID must be a string.',
+  }).nonempty('Car owner ID cannot be empty.'),
+  priceTotal: number({
+    required_error:
+      'A car price is required. Please provide a car price for the car you are selling.',
+    invalid_type_error: 'Car price must be a number.',
+  }).min(0, 'Price must be a positive number.'),
+  seats: number({
+    required_error:
+      'A car seats is required. Please provide a car seats for the car you are selling.',
+    invalid_type_error: 'Car seats must be a number.',
+  })
+    .min(2, 'Car must have at least two seats.')
+    .max(8, 'Car must have at least eight seats.'),
+  year: number({
+    required_error:
+      'A car year is required. Please provide a year for the car you are selling.',
+    invalid_type_error: 'Car year must be a number.',
+  }).refine((value) => value >= 2000 && value <= new Date().getFullYear(), {
+    message: `Year must be a valid year between 2000 and ${new Date().getFullYear()}.`,
   }),
 });
+
+// Create a TypeScript type from the schema.
+export type CreateCarSchemaType = z.infer<typeof createCarSchema>;
 
 export default createCarSchema;
--- models/car/validators/updateCar.schema.ts
+++ models/car/validators/updateCar.schema.ts
@@ -1,37 +1,86 @@
-import carColours from '../../../api/lib/constants/carColours';
-import { number, object, string } from 'zod';
+import { number, object, string, z } from 'zod';
+import carColourValues from '../values/carColourValues';
+import carGearboxValues from '../values/carGearboxValues';
+import carFuelTypeValues from '../values/carFuelTypeValues';
+import carDriveTypeValues from '../values/carDriveTypeValues';
+import carEngineSizeValues from '../values/carEngineSizeValues';
 
-// Update car schema is a validation schema defining the acceptable fields required.
-
+// Zod validation schema defines fields required for updating a car.
 const updateCarSchema = object({
+  colour: string({
+    invalid_type_error: 'Car colour must be a string.',
+  })
+    .nonempty('Car colour cannot be empty.')
+    .refine((value) => carColourValues.includes(value), {
+      message: `Car colour must be one of ${carColourValues}.`,
+    }),
+  doors: number({
+    invalid_type_error: 'Car doors must be a string.',
+  })
+    .min(2, 'Car must have at least two doors.')
+    .max(5, 'Car must have at least five doors.'),
+  description: string({
+    invalid_type_error: 'Car description must be a string.',
+  })
+    .nonempty('Car description cannot be empty.')
+    .optional(),
+  driveType: string({
+    invalid_type_error: 'Car drive type must be a string.',
+  })
+    .nonempty('Car drive type cannot be empty.')
+    .refine((value) => carDriveTypeValues.includes(value), {
+      message: `Car drive type must be one of ${carDriveTypeValues}.`,
+    }),
+  engineSizeTotal: string({
+    invalid_type_error: 'Car engine size must be a string.',
+  })
+    .nonempty('Car engine size cannot be empty.')
+    .refine((value) => carEngineSizeValues.includes(value), {
+      message: `Car engine size must be one of ${carEngineSizeValues}.`,
+    }),
+  fuelType: string({
+    invalid_type_error: 'Car fuel type must be a string.',
+  })
+    .nonempty('Car fuel type cannot be empty.')
+    .refine((value) => carFuelTypeValues.includes(value), {
+      message: `Car fuel type must be one of ${carFuelTypeValues}.`,
+    }),
+  gearbox: string({
+    invalid_type_error: 'Car gearbox must be a string.',
+  })
+    .nonempty('Car Gearbox cannot be empty.')
+    .refine((value) => carGearboxValues.includes(value), {
+      message: `Car gearbox must be one of ${carGearboxValues}.`,
+    }),
   make: string({
     invalid_type_error: 'Car make must be a string.',
-  }).optional(),
+  }).nonempty('Car make cannot be empty.'),
+
+  mileageTotal: number({
+    invalid_type_error: 'Car mileage must be a number.',
+  }).min(0, 'Mileage must be a positive number.'),
   model: string({
     invalid_type_error: 'Car model must be a string.',
-  }).optional(),
-  colour: string({
-    invalid_type_error: 'Colour must be a string.',
-  })
-    .optional()
-    .refine((value) => value === undefined || carColours.includes(value), {
-      message: 'Colour must be one of "blue", "red", "orange", or "green".',
-    }),
-  year: number({
-    invalid_type_error: 'Year must be a number.',
-  })
-    .refine(
-      (value) =>
-        value === undefined ||
-        (value >= 1950 && value <= new Date().getFullYear()),
-      {
-        message: `Year must be a valid year between 1950 and ${new Date().getFullYear()}.`,
-      },
-    )
-    .optional(),
+  }).nonempty('Car model cannot be empty.'),
   ownerId: string({
-    invalid_type_error: 'Owner ID must be a string.',
-  }).optional(),
+    invalid_type_error: 'Car owner ID must be a string.',
+  }).nonempty('Car owner ID cannot be empty.'),
+  priceTotal: number({
+    invalid_type_error: 'Car price must be a number.',
+  }).min(0, 'Price must be a positive number.'),
+  seats: number({
+    invalid_type_error: 'Car seats must be a number.',
+  })
+    .min(2, 'Car must have at least two seats.')
+    .max(8, 'Car must have at least eight seats.'),
+  year: number({
+    invalid_type_error: 'Car year must be a number.',
+  }).refine((value) => value >= 2000 && value <= new Date().getFullYear(), {
+    message: `Year must be a valid year between 2000 and ${new Date().getFullYear()}.`,
+  }),
 });
+
+// Create a TypeScript type from the schema.
+export type UpdateCarSchemaType = z.infer<typeof updateCarSchema>;
 
 export default updateCarSchema;
