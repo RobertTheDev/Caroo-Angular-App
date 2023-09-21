a4e0382bc3e31f3b90e98a9f5fae4cacc51d0d86
RobertTheDev <robertthedev@gmail.com>
2023-09-21 19:10:53 +0100

    feat: added validator

--- api/middlewares/auth/isEmailAvailable.ts
+++ api/middlewares/auth/isEmailAvailable.ts
@@ -1,6 +1,8 @@
-import { UserService } from 'api/providers/prisma/user.service';
+import UserPrismaService from 'api/providers/prisma/user.service';
+import winstonLogger from 'api/utils/winstonLogger';
 import { Request, Response, NextFunction } from 'express';
 import { ReasonPhrases, StatusCodes } from 'http-status-codes';
+import validEmailSchema from 'models/shared/validators/validEmail.schema';
 
 // This middleware ensures an email is available and not already in use before performing a server request.
 
@@ -10,30 +12,45 @@ export default async function isEmailAvailable(
   next: NextFunction,
 ) {
   try {
-    // Get email from request body.
-    const { email } = req.body;
+    // Get the request body.
+    const { body } = req;
+
+    // Validate the email address from the body.
+    const validation = await validEmailSchema.safeParseAsync(body);
+
+    // If validation is unsuccessful send an error response with validation error.
+    if (!validation.success) {
+      return res.status(StatusCodes.BAD_REQUEST).send({
+        message: validation.error.issues[0].message,
+      });
+    }
+
+    // Get email address from validation data.
+    const { emailAddress } = validation.data;
 
     // Declare and use user service.
-    const userService = new UserService();
+    const userPrismaService = new UserPrismaService();
 
     // Check email is available by checking a user does not exist in database.
-    const checkEmailIsAvailable = await userService.findOneByEmail(email);
+    const checkEmailIsAvailable = await userPrismaService.findOneByEmailAddress(
+      emailAddress,
+    );
 
-    // If not user exists with the email address then continue.
+    // If no user exists with the email address then continue.
     if (!checkEmailIsAvailable) {
       return next();
     }
 
     // If user exists throw a bad request error.
     return res.status(StatusCodes.BAD_REQUEST).send({
-      message: ReasonPhrases.BAD_REQUEST,
-      error: `The user with email ${email} already exists. Please use a different email address.`,
+      message: `The user with email ${emailAddress} already exists. Please use a different email address.`,
     });
   } catch (error) {
+    // Log the error.
+    winstonLogger.error(`Error checking if email address is available:`, error);
     // Catch and return an error if found.
     return res.status(StatusCodes.INTERNAL_SERVER_ERROR).send({
       message: ReasonPhrases.INTERNAL_SERVER_ERROR,
-      error,
     });
   }
 }
