7d0ffd6358de400bd03d83996ad0a2097cff97cd
RobertTheDev <robertthedev@gmail.com>
2023-09-21 02:58:31 +0100

    feat: defined car request values

---
+++ models/carRequest/values/carRequestStatusValues.ts
@@ -0,0 +1,10 @@
+// Defines the possible values for a car request status.
+
+const carRequestStatusValues = [
+  'SellerApproved',
+  'BuyerDeclined',
+  'Requested',
+  'Rejected',
+];
+
+export default carRequestStatusValues;
