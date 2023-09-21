b9960a565648efeb94367279e220bcb7cf0ec674
RobertTheDev <robertthedev@gmail.com>
2023-09-21 19:07:46 +0100

    feat: created winston logger

---
+++ api/utils/winstonLogger.ts
@@ -0,0 +1,28 @@
+import { createLogger, transports, format } from 'winston';
+
+const winstonLogger = createLogger({
+  level: 'info',
+  format: format.json(),
+  defaultMeta: { service: 'user-service' },
+  transports: [
+    //
+    // - Write all logs with importance level of `error` or less to `error.log`
+    // - Write all logs with importance level of `info` or less to `combined.log`
+    //
+    new transports.File({ filename: 'error.log', level: 'error' }),
+    new transports.File({ filename: 'combined.log' }),
+  ],
+});
+
+//
+// If we're not in production then log to the `console` with the format:
+// `${info.level}: ${info.message} JSON.stringify({ ...rest }) `
+//
+if (process.env['NODE_ENV'] !== 'production') {
+  winstonLogger.add(
+    new transports.Console({
+      format: format.simple(),
+    }),
+  );
+}
+export default winstonLogger;
