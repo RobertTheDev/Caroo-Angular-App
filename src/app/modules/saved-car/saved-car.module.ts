dbe76c48ba234f00f650203c7252c71487b6573e
RobertTheDev <robertthedev@gmail.com>
2023-09-21 06:08:51 +0100

    ci: deleted and removed saved cars list

--- src/app/modules/saved-car/saved-car.module.ts
+++ src/app/modules/saved-car/saved-car.module.ts
@@ -1,10 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SavedCarsViewComponent } from './views/saved-cars-view/saved-cars-view.component';
-import { SavedCarsListComponent } from './components/saved-cars-list/saved-cars-list.component';
 
 @NgModule({
-  declarations: [SavedCarsViewComponent, SavedCarsListComponent],
+  declarations: [SavedCarsViewComponent],
   imports: [CommonModule],
   exports: [SavedCarsViewComponent],
 })
