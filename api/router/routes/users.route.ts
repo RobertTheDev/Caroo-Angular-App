ad6f24153ceb0705a26a703067d11c090244569a
RobertTheDev <robertthedev@gmail.com>
2023-09-22 23:23:43 +0100

    ci: removed verify email routes

--- api/router/routes/users.route.ts
+++ api/router/routes/users.route.ts
@@ -1,7 +1,10 @@
+import sendEmailVerificationToken from 'api/controllers/account/sendEmailVerificationToken';
+import verifyEmailWithToken from 'api/controllers/account/verifyEmailWithToken';
 import getUserByEmail from 'api/controllers/user/getUserByEmailAddress';
 import getUserById from 'api/controllers/user/getUserById';
 import getUsers from 'api/controllers/user/getUsers';
 import updateUserById from 'api/controllers/user/updateUserById';
+import isEmailVerificationTokenExpired from 'api/middlewares/account/isEmailVerificationTokenExpired';
 import isAuthenticated from 'api/middlewares/auth/isAuthenticated';
 import { Router } from 'express';
 
@@ -18,5 +21,18 @@ usersRouter.get('/:id', getUserById);
 usersRouter.get('/email/:email', getUserByEmail);
 
 usersRouter.put('/:id', isAuthenticated, updateUserById);
+
+usersRouter.put(
+  '/verify-email-address/:emailVerificationToken',
+  isAuthenticated,
+  isEmailVerificationTokenExpired,
+  verifyEmailWithToken,
+);
+
+usersRouter.post(
+  '/verify-email-address/send-token',
+  isAuthenticated,
+  sendEmailVerificationToken,
+);
 
 export default usersRouter;
