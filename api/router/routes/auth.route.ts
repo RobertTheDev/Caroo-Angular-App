80f5df53315a3d202ddd9212e82e70bce5afef77
RobertTheDev <robertthedev@gmail.com>
2023-09-22 05:52:45 +0100

    feat: created password reset routes

--- api/router/routes/auth.route.ts
+++ api/router/routes/auth.route.ts
@@ -1,8 +1,4 @@
 import { Router } from 'express';
-import changeEmail from 'api/controllers/settings/changeEmail';
-import changePassword from 'api/controllers/settings/changePassword';
-import closeAccount from 'api/controllers/settings/closeAccount';
-import forgotPassword from 'api/controllers/auth/forgotPassword';
 import getAuthenticatedUser from 'api/controllers/auth/getAuthenticatedUser';
 import login from 'api/controllers/auth/login';
 import logout from 'api/controllers/auth/logout';
@@ -10,6 +6,10 @@ import signUp from 'api/controllers/auth/signUp';
 import isAuthenticated from 'api/middlewares/auth/isAuthenticated';
 import isUserNotSignedIn from 'api/middlewares/auth/isUserNotSignedIn';
 import isEmailAvailable from 'api/middlewares/auth/isEmailAvailable';
+import isPasswordCorrect from 'api/middlewares/auth/isPasswordCorrect';
+import resetPasswordWithToken from 'api/controllers/auth/resetPasswordWithToken';
+import sendResetPasswordToken from 'api/controllers/auth/sendResetPasswordToken';
+import isPasswordResetTokenExpired from 'api/middlewares/auth/isPasswordResetTokenExpired';
 
 // Define auth router using the express router.
 const authRouter = Router();
@@ -17,20 +17,25 @@ const authRouter = Router();
 // Auth router defines all the routes used in modifying user authentication.
 // Auth router is exported into the app router file.
 
-authRouter.delete('/close-account', isAuthenticated, closeAccount);
+authRouter.get('/authenticated-user', isAuthenticated, getAuthenticatedUser);
+
+authRouter.post('/login', isUserNotSignedIn, isPasswordCorrect, login);
 
 authRouter.delete('/logout', isAuthenticated, logout);
 
-authRouter.get('/authenticated-user', isAuthenticated, getAuthenticatedUser);
+authRouter.put(
+  '/reset-password/:resetPasswordToken',
+  isUserNotSignedIn,
+  isPasswordResetTokenExpired,
+  resetPasswordWithToken,
+);
 
-authRouter.post('/login', isUserNotSignedIn, login);
+authRouter.post(
+  '/reset-password/send-token',
+  isUserNotSignedIn,
+  sendResetPasswordToken,
+);
 
 authRouter.post('/sign-up', isUserNotSignedIn, isEmailAvailable, signUp);
-
-authRouter.put('/change-email', isAuthenticated, changeEmail);
-
-authRouter.put('/change-password', isAuthenticated, changePassword);
-
-authRouter.put('/forgot-password', isUserNotSignedIn, forgotPassword);
 
 export default authRouter;
