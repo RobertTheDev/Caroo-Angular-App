d19dddbb8d229203d271f430559b1e8f5c105f9b
RobertTheDev <robertthedev@gmail.com>
2023-09-22 23:23:58 +0100

    feat: added the account router

--- api/router/index.ts
+++ api/router/index.ts
@@ -5,12 +5,14 @@ import userAvatarsRouter from './routes/userAvatars';
 import savedCarsRouter from './routes/savedCars.route';
 import carsRouter from './routes/cars.route';
 import authRouter from './routes/auth.route';
+import accountRouter from './routes/account.route';
 
 // App router defines all the routes used in the application.
 // App router is exported into the main server file.
 
 const appRouter = Router();
 
+appRouter.use('/account', accountRouter);
 appRouter.use('/auth', authRouter);
 appRouter.use('/cars', carsRouter);
 appRouter.use('/car-owners', carOwnersRouter);
