9db3bcdfc3e5828dcdf37cd441ff04cb26173993
RobertTheDev <robertthedev@gmail.com>
2023-09-22 21:30:13 +0100

    feat: added update user by id route

--- api/router/routes/users.route.ts
+++ api/router/routes/users.route.ts
@@ -1,6 +1,8 @@
 import getUserByEmail from 'api/controllers/user/getUserByEmailAddress';
 import getUserById from 'api/controllers/user/getUserById';
 import getUsers from 'api/controllers/user/getUsers';
+import updateUserById from 'api/controllers/user/updateUserById';
+import isAuthenticated from 'api/middlewares/auth/isAuthenticated';
 import { Router } from 'express';
 
 // User router defines all the routes used in modifying users data.
@@ -14,5 +16,7 @@ usersRouter.get('/', getUsers);
 usersRouter.get('/:id', getUserById);
 
 usersRouter.get('/email/:email', getUserByEmail);
+
+usersRouter.put('/:id', isAuthenticated, updateUserById);
 
 export default usersRouter;
