a065dfc6cff8453db8882f14a47dd04ce78cebcb
RobertTheDev <robertthedev@gmail.com>
2023-09-23 03:30:52 +0100

    ci: deleted create car and car owner modules

--- src/app/modules/create-car/components/create-car-form/create-car-form.component.ts
+++
@@ -1,7 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-create-car-form',
-  templateUrl: './create-car-form.component.html',
-})
-export class CreateCarFormComponent {}
