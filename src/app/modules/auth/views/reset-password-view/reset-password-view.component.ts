23765bebc9c9a87f2c507423b147ae67791b3c31
RobertTheDev <robertthedev@gmail.com>
2023-09-23 23:58:09 +0100

    ci: removed css

--- src/app/modules/auth/views/reset-password-view/reset-password-view.component.ts
+++ src/app/modules/auth/views/reset-password-view/reset-password-view.component.ts
@@ -3,6 +3,5 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-reset-password-view',
   templateUrl: './reset-password-view.component.html',
-  styleUrls: ['./reset-password-view.component.css'],
 })
 export class ResetPasswordViewComponent {}
--- src/app/modules/auth/views/send-password-reset-page/send-password-reset-page.component.ts
+++ src/app/modules/auth/views/send-password-reset-page/send-password-reset-page.component.ts
@@ -3,6 +3,5 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-send-password-reset-page',
   templateUrl: './send-password-reset-page.component.html',
-  styleUrls: ['./send-password-reset-page.component.css'],
 })
 export class SendPasswordResetPageComponent {}
