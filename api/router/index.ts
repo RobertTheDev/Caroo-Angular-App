29ceb344ca8f679988e03eea1a40c362f93e0ccf
RobertTheDev <robertthedev@gmail.com>
2023-09-23 01:56:45 +0100

    feat: added car imaged and requests routes

--- api/router/index.ts
+++ api/router/index.ts
@@ -1,10 +1,11 @@
 import { Router } from 'express';
 import usersRouter from './routes/users.route';
-import userAvatarsRouter from './routes/userAvatars';
 import savedCarsRouter from './routes/savedCars.route';
 import carsRouter from './routes/cars.route';
 import authRouter from './routes/auth.route';
 import accountRouter from './routes/account.route';
+import carImagesRouter from './routes/carImages.route';
+import carRequestsRouter from './routes/carRequests.route';
 
 // App router defines all the routes used in the application.
 // App router is exported into the main server file.
@@ -13,9 +14,10 @@ const appRouter = Router();
 
 appRouter.use('/account', accountRouter);
 appRouter.use('/auth', authRouter);
+appRouter.use('/car-images', carImagesRouter);
+appRouter.use('/car-requests', carRequestsRouter);
 appRouter.use('/cars', carsRouter);
 appRouter.use('/saved-cars', savedCarsRouter);
-appRouter.use('/user-avatars', userAvatarsRouter);
 appRouter.use('/users', usersRouter);
 
 export default appRouter;
