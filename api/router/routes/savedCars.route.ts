3b2e023a852b8b9ae6b3e27557d9d24b93254de7
RobertTheDev <robertthedev@gmail.com>
2023-09-23 01:36:36 +0100

    feat: defined saved car routes

--- api/router/routes/savedCars.route.ts
+++ api/router/routes/savedCars.route.ts
@@ -1,3 +1,12 @@
+import createSavedCar from 'api/controllers/savedCar/createSavedCar';
+import deleteSavedCarById from 'api/controllers/savedCar/deleteSavedCarById';
+import deleteSavedCarsByCarId from 'api/controllers/savedCar/deleteSavedCarsByCarId';
+import deleteSavedCarsByUserId from 'api/controllers/savedCar/deleteSavedCarsByUserId';
+import getSavedCarById from 'api/controllers/savedCar/getSavedCarById';
+import getSavedCars from 'api/controllers/savedCar/getSavedCars';
+import getSavedCarsByCarId from 'api/controllers/savedCar/getSavedCarsByCarId';
+import getSavedCarsByUserId from 'api/controllers/savedCar/getSavedCarsByUserId';
+import isAuthenticated from 'api/middlewares/auth/isAuthenticated';
 import { Router } from 'express';
 
 // Saved cars router defines all the routes used in modifying saved cars data.
@@ -6,8 +15,26 @@ import { Router } from 'express';
 // Define saved cars router using the express router.
 const savedCarsRouter = Router();
 
-savedCarsRouter.get('/saved-cars', (_req, res) => {
-  res.status(200).send('saved cars');
-});
+savedCarsRouter.delete('/:id', isAuthenticated, deleteSavedCarById);
+
+savedCarsRouter.delete('/car/:carId', isAuthenticated, deleteSavedCarsByCarId);
+
+savedCarsRouter.delete(
+  '/user/:userId',
+  isAuthenticated,
+  deleteSavedCarsByUserId,
+);
+
+savedCarsRouter.get('/', getSavedCars);
+
+savedCarsRouter.get('/:id', getSavedCarById);
+
+savedCarsRouter.get('/car/:carId', getSavedCarsByCarId);
+
+savedCarsRouter.get('/user/:userId', getSavedCarsByUserId);
+
+savedCarsRouter.get('/:id', getSavedCarById);
+
+savedCarsRouter.post('/save-car', isAuthenticated, createSavedCar);
 
 export default savedCarsRouter;
