0a8e4cd4feab2bc88ef03e1c79b4ede5cb10d81d
RobertTheDev <robertthedev@gmail.com>
2023-09-24 01:02:47 +0100

    feat: added create car handler

--- src/app/services/car/car.service.ts
+++ src/app/services/car/car.service.ts
@@ -9,7 +9,19 @@ import CarWithImages from 'src/app/types/CarWithImages';
 export class CarService {
   private apiUrl = 'http://localhost:4200/api/cars/';
 
+  private options = { withCredentials: true };
+
   constructor(private http: HttpClient) {}
+
+  createCar(
+    data: Partial<{
+      make: string | null;
+      model: string | null;
+      colour: string | null;
+    }>,
+  ) {
+    return this.http.post<void>(this.apiUrl, data, this.options);
+  }
 
   getCars(): Observable<{ data: CarWithImages[] }> {
     return this.http.get<{ data: CarWithImages[] }>(this.apiUrl);
