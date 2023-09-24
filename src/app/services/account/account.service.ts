07976b38a85e91076b198bc8eabbee6132ee52e6
RobertTheDev <robertthedev@gmail.com>
2023-09-24 01:43:12 +0100

    feat: created account service handlers

--- src/app/services/account/account.service.ts
+++ src/app/services/account/account.service.ts
@@ -1,6 +1,75 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
-export class AccountService {}
+export class AccountService {
+  private apiUrl = 'http://localhost:4200/api/account/';
+
+  private options = { withCredentials: true };
+
+  constructor(private http: HttpClient) {}
+
+  changeEmailAddress(
+    data: Partial<{
+      emailAddress: string;
+    }>,
+  ): Observable<void> {
+    return this.http.put<void>(
+      `${this.apiUrl}/change-email-adddress`,
+      data,
+      this.options,
+    );
+  }
+
+  changePassword(
+    data: Partial<{
+      password: string;
+    }>,
+  ): Observable<void> {
+    return this.http.put<void>(
+      `${this.apiUrl}/change-password`,
+      data,
+      this.options,
+    );
+  }
+
+  closeAccount(
+    data: Partial<{
+      close: string;
+    }>,
+  ): Observable<void> {
+    return this.http.put<void>(
+      `${this.apiUrl}/close-account`,
+      data,
+      this.options,
+    );
+  }
+
+  sendEmailVerificationToken(
+    data: Partial<{
+      emailAddress: string;
+    }>,
+  ): Observable<void> {
+    return this.http.put<void>(
+      `${this.apiUrl}/verify-email/send-token`,
+      data,
+      this.options,
+    );
+  }
+
+  verifyEmailWithToken(
+    token: string,
+    data: Partial<{
+      emailAddress: string;
+    }>,
+  ): Observable<void> {
+    return this.http.put<void>(
+      `${this.apiUrl}/verify-email/${token}`,
+      data,
+      this.options,
+    );
+  }
+}
