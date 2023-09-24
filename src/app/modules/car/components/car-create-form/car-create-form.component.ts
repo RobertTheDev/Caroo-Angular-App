d8f26112c0f7cd5df4294aa71738b380a85bf9cb
RobertTheDev <robertthedev@gmail.com>
2023-09-24 01:03:04 +0100

    feat: created create car form with functionality

--- src/app/modules/car/components/car-create-form/car-create-form.component.ts
+++ src/app/modules/car/components/car-create-form/car-create-form.component.ts
@@ -1,5 +1,14 @@
 import { Component } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
+import carColourValues from 'models/car/values/carColourValues';
+import carDriveTypeValues from 'models/car/values/carDriveTypeValues';
+import carEngineSizeValues from 'models/car/values/carEngineSizeValues';
+import carFuelTypeValues from 'models/car/values/carFuelTypeValues';
+import carGearboxValues from 'models/car/values/carGearboxValues';
+import carMakeAndModelsValues from 'models/car/values/carMakeAndModelsValues';
+import generateCarYears from 'models/car/values/carYearValues';
+import { Subscription } from 'rxjs';
+import { CarService } from 'src/app/services/car/car.service';
 
 @Component({
   selector: 'app-car-create-form',
@@ -8,27 +17,30 @@ import { FormBuilder, Validators } from '@angular/forms';
 })
 export class CarCreateFormComponent {
   // Use the angular form builder.
-  constructor(private fb: FormBuilder) {}
+  constructor(
+    // Angular form builder is used to build forms in Angular with less code.
+    private formBuilder: FormBuilder,
+    // Calls our car service to get access to our car function helpers.
+    private carService: CarService,
+  ) {}
 
-  images: string[] = new Array(6).fill('');
+  // Variables are used for handling form states - submitted, loading and errors.
+  formErrorMessage: string | null = null;
+  formLoading = false;
+  formSubmitted = false;
 
-  replaceImage(index: number) {
-    // Handle replacing the image at the specified index
-    const newImageUrl = prompt('Enter image URL:');
-    if (newImageUrl) {
-      this.images[index] = newImageUrl;
-    }
-  }
-
-  deleteImage(index: number) {
-    // Handle deleting the image at the specified index
-    this.images[index] = '';
-  }
+  carColourValues = carColourValues;
+  carFuelTypeValues = carFuelTypeValues;
+  carGearboxValues = carGearboxValues;
+  carDriveTypeValues = carDriveTypeValues;
+  carEngineSizeValues = carEngineSizeValues;
+  carMakeAndModelsValues = carMakeAndModelsValues;
+  years = generateCarYears();
 
   // Form fields.
-  form = this.fb.group({
+  createCarForm = this.formBuilder.group({
     make: [null, [Validators.required]],
-    model: [null, [Validators.required]],
+    model: ['', [Validators.required]],
     colour: [null, [Validators.required]],
     year: [null, [Validators.required]],
     fuelType: [null, [Validators.required]],
@@ -42,20 +54,64 @@ export class CarCreateFormComponent {
     description: [null, [Validators.required]],
   });
 
-  // Helper function to access form controls.
-  get formControls() {
-    return this.form.controls;
+  get selectedMake() {
+    return this.createCarForm.get('make')?.value || null;
   }
 
-  // Handle submit after fields are compelete and form submission.
-  handleSubmit() {
-    // If form is valid.
-    if (this.form.valid) {
-      alert('Form submitted successfully:\n' + JSON.stringify(this.form.value));
+  getModelOptions(make: string): string[] {
+    const selectedMake = this.carMakeAndModelsValues.find(
+      (carMake) => carMake.make === make,
+    );
+    return selectedMake ? selectedMake.models : [];
+  }
+
+  onMakeChange(event: Event) {
+    const selectedMake = (event.target as HTMLSelectElement).value;
+
+    // Update the models based on the selected make
+    const models = this.getModelOptions(selectedMake);
+    const defaultModel = models.length > 0 ? models[0] : null;
+
+    // Update the model control
+    this.createCarForm.patchValue({
+      model: defaultModel,
+    });
+  }
+
+  // Helper function to access form controls.
+  get formControls() {
+    return this.createCarForm.controls;
+  }
+
+  // Create car function calls the create car handler from our car service.
+  // The function handles validation and wont submit until fields are valid.
+  // The function handles errors and displays error response messages.
+  handleCreateCar(): Subscription | undefined {
+    // When function is called set form submitted to true and error message to null.
+    this.formSubmitted = true;
+    this.formErrorMessage = null;
+
+    // If form is invalid do not continue and return nothing.
+    if (this.createCarForm.invalid) {
+      return;
     }
-    // If form is invalid.
-    else {
-      alert('Please correct the errors in the form.');
-    }
+
+    // Get the value data from the sign up form.
+    const { value } = this.createCarForm;
+
+    // Start loading while form is being processed.
+    this.formLoading = true;
+
+    return this.carService.createCar(value).subscribe({
+      // If form has successfully created a car - stop the form loading.
+      next: () => {
+        this.formLoading = false;
+      },
+      // If an error contain the error message in the variable. Stop form loading.
+      error: (error) => {
+        this.formLoading = false;
+        this.formErrorMessage = error.error.statusMessage;
+      },
+    });
   }
 }
