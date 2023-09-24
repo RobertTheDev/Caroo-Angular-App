45322d0367917f2e901e2acc67809ed81facb9f8
RobertTheDev <robertthedev@gmail.com>
2023-09-24 03:37:12 +0100

    feat: created saved cars list component

--- src/app/modules/saved-car/saved-car.module.ts
+++ src/app/modules/saved-car/saved-car.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SavedCarsViewComponent } from './views/saved-cars-view/saved-cars-view.component';
+import { SavedCarsListComponent } from './components/saved-cars-list/saved-cars-list.component';
 
 @NgModule({
-  declarations: [SavedCarsViewComponent],
+  declarations: [SavedCarsViewComponent, SavedCarsListComponent],
   imports: [CommonModule],
   exports: [SavedCarsViewComponent],
 })
