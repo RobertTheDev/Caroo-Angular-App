3a1b103d22496a53e23a2c2ad37d0a5b2c6c55ce
RobertTheDev <robertthedev@gmail.com>
2023-09-24 01:20:05 +0100

    feat: defined saved car service handlers

--- src/app/services/savedCar/saved-car.service.ts
+++ src/app/services/savedCar/saved-car.service.ts
@@ -1,6 +1,60 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { SavedCar } from '@prisma/client';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
-export class SavedCarService {}
+export class SavedCarService {
+  private apiUrl = 'http://localhost:4200/api/saved-cars/';
+
+  private options = { withCredentials: true };
+
+  constructor(private http: HttpClient) {}
+
+  createSavedCar(
+    data: Partial<{
+      carId: string | null;
+      userId: string | null;
+    }>,
+  ): Observable<void> {
+    return this.http.post<void>(this.apiUrl, data, this.options);
+  }
+
+  deleteSavedCarById(id: string) {
+    return this.http.delete<void>(`${this.apiUrl}/${id}`, this.options);
+  }
+
+  deleteSavedCarsByUserId(userId: string) {
+    return this.http.delete<void>(
+      `${this.apiUrl}/user/${userId}`,
+      this.options,
+    );
+  }
+
+  deleteSavedCarsByCarId(carId: string) {
+    return this.http.delete<void>(`${this.apiUrl}/car/${carId}`, this.options);
+  }
+
+  getSavedCarById(id: string): Observable<{ data: SavedCar | null }> {
+    return this.http.get<{ data: SavedCar | null }>(
+      `${this.apiUrl}/${id}`,
+      this.options,
+    );
+  }
+
+  getSavedCarsByUserId(userId: string): Observable<{ data: SavedCar[] }> {
+    return this.http.get<{ data: SavedCar[] }>(
+      `${this.apiUrl}/user/${userId}`,
+      this.options,
+    );
+  }
+
+  getSavedCarsByCarId(carId: string): Observable<{ data: SavedCar[] }> {
+    return this.http.get<{ data: SavedCar[] }>(
+      `${this.apiUrl}/car/${carId}`,
+      this.options,
+    );
+  }
+}
