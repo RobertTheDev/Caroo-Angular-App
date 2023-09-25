617a61e3eb3233e980973236a678f90f23db86d9
RobertTheDev <robertthedev@gmail.com>
2023-09-25 03:48:39 +0100

    feat: created profile menu links

---
+++ src/app/lib/links/profileMenuLinks.ts
@@ -0,0 +1,26 @@
+import Link from 'models/config/types/Link';
+
+const profileMenuLinks: Link[] = [
+  {
+    name: 'View Profile',
+    path: '/verify-email-address',
+  },
+  {
+    name: 'Edit Profile',
+    path: '/update-email',
+  },
+  {
+    name: 'My Requests',
+    path: '/close-account',
+  },
+  {
+    name: 'My Car Listings',
+    path: '/close-account',
+  },
+  {
+    name: 'Account Settings',
+    path: '/update-email',
+  },
+];
+
+export default profileMenuLinks;
