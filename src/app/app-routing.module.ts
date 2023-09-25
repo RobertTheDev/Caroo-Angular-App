ada94d29417341844bdb9ca225a5008b97448fa0
RobertTheDev <robertthedev@gmail.com>
2023-09-25 02:48:23 +0100

    feat: added seller route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { NotFoundViewComponent } from './modules/not-found/views/not-found-view/not-found-view.component';
 import { SavedCarsViewComponent } from './modules/saved-car/views/saved-cars-view/saved-cars-view.component';
 import { CarsViewComponent } from './modules/car/views/cars-view/cars-view.component';
+import { SellerViewComponent } from './modules/seller/views/seller-view/seller-view.component';
 
 const routes: Routes = [
   { path: '', component: CarsViewComponent },
@@ -42,6 +43,7 @@ const routes: Routes = [
       ),
   },
   { path: 'saved-cars', component: SavedCarsViewComponent },
+  { path: 'sellers/:emailAddress', component: SellerViewComponent },
   { path: '**', component: NotFoundViewComponent },
 ];
 
