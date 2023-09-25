3b7066443efce026e2533399ab3e72f25d52f50e
RobertTheDev <robertthedev@gmail.com>
2023-09-25 02:32:46 +0100

    feat: added how it works route

--- src/app/lib/links/footerLinks.ts
+++ src/app/lib/links/footerLinks.ts
@@ -4,7 +4,10 @@ const footerLinks: Link[] = [
   { name: 'About', path: 'about' },
   { name: 'Accessibility Statement', path: 'accessibility-statement' },
   { name: 'Cookie Policy', path: 'cookie-policy' },
+  { name: 'Find Cars', path: '' },
+  { name: 'How it works', path: 'how-it-works' },
   { name: 'Privacy Policy', path: 'privacy-policy' },
+  { name: 'Sell a Car', path: 'sell-a-car' },
   { name: 'Terms and Conditions', path: 'terms-and-conditions' },
 ];
 
--- src/app/lib/links/headerLinks.ts
+++ src/app/lib/links/headerLinks.ts
@@ -2,8 +2,9 @@ import Link from 'models/config/types/Link';
 
 const headerLinks: Link[] = [
   { name: 'Find Cars', path: '' },
-  { name: 'Sell A Car', path: 'sell-a-car' },
+  { name: 'Sell a Car', path: 'sell-a-car' },
   { name: 'About Caroo', path: 'about' },
+  { name: 'How it works', path: 'how-it-works' },
 ];
 
 export default headerLinks;
