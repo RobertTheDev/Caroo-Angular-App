02eee0c70c17ed9d44e9f4b64980f3a819873dbb
RobertTheDev <robertthedev@gmail.com>
2023-09-25 06:41:27 +0100

    feat: created close account form

---
+++ src/app/modules/account/components/close-account-form/close-account-form.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-close-account-form',
+  templateUrl: './close-account-form.component.html',
+  styleUrls: ['./close-account-form.component.css']
+})
+export class CloseAccountFormComponent {
+
+}
