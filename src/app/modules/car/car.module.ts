a7f64694c55fdbc17b13b2fc657450e7b76802ce
RobertTheDev <robertthedev@gmail.com>
2023-09-25 06:47:57 +0100

    ci: deleted and removed filters cars view

--- src/app/modules/car/car.module.ts
+++ src/app/modules/car/car.module.ts
@@ -9,7 +9,6 @@ import { CarsViewComponent } from './views/cars-view/cars-view.component';
 import { CarViewComponent } from './views/car-view/car-view.component';
 import { CreateCarViewComponent } from './views/create-car-view/create-car-view.component';
 import { EditCarViewComponent } from './views/edit-car-view/edit-car-view.component';
-import { FilterCarsViewComponent } from './views/filter-cars-view/filter-cars-view.component';
 import { CarCardComponent } from './components/car-card/car-card.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { SharedModule } from '../shared/shared.module';
@@ -23,7 +22,6 @@ import { SharedModule } from '../shared/shared.module';
     CarViewComponent,
     CreateCarViewComponent,
     EditCarViewComponent,
-    FilterCarsViewComponent,
     CarCardComponent,
   ],
   imports: [CommonModule, RouterModule, FontAwesomeModule, SharedModule],
@@ -32,7 +30,6 @@ import { SharedModule } from '../shared/shared.module';
     CarViewComponent,
     CreateCarViewComponent,
     EditCarViewComponent,
-    FilterCarsViewComponent,
   ],
   providers: [CarService],
 })
--- src/app/modules/car/views/filter-cars-view/filter-cars-view.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-filter-cars-view',
-  templateUrl: './filter-cars-view.component.html',
-  styleUrls: ['./filter-cars-view.component.css'],
-})
-export class FilterCarsViewComponent {}
