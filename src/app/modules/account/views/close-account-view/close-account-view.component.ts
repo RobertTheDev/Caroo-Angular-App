2559cf36ecf0faef5d09b94d0ac7861f7b17c8b8
RobertTheDev <robertthedev@gmail.com>
2023-09-25 06:41:55 +0100

    feat: added close account form component

--- src/app/modules/account/views/close-account-view/close-account-view.component.ts
+++ src/app/modules/account/views/close-account-view/close-account-view.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
+import { MetaService } from 'src/app/services/meta/meta.service';
 
 @Component({
   selector: 'app-close-account-view',
   templateUrl: './close-account-view.component.html',
-  styleUrls: ['./close-account-view.component.css']
 })
 export class CloseAccountViewComponent {
-
+  constructor(metaService: MetaService) {}
 }
