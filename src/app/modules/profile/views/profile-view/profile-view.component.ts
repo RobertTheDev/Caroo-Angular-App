abb18ddc466f170040856fc71df8fe50a79c0c2b
RobertTheDev <robertthedev@gmail.com>
2023-09-25 09:24:05 +0100

    feat: added form handling

--- src/app/modules/profile/views/profile-view/profile-view.component.ts
+++ src/app/modules/profile/views/profile-view/profile-view.component.ts
@@ -1,14 +1,35 @@
 import { Component, OnInit } from '@angular/core';
+import IUser from 'models/user/types/User';
+
 import { MetaService } from 'src/app/services/meta/meta.service';
+import { ProfileService } from 'src/app/services/profile/profile.service';
 
 @Component({
   selector: 'app-profile-view',
   templateUrl: './profile-view.component.html',
 })
 export class ProfileViewComponent implements OnInit {
-  constructor(private metaService: MetaService) {}
+  constructor(
+    private metaService: MetaService,
+    private profileService: ProfileService,
+  ) {}
+
+  profile: IUser | null = null;
+  loading = true;
 
   ngOnInit() {
     this.metaService.setMeta('Profile', 'Description.', 'Keywords', 'profile');
+
+    this.profileService.getProfile().subscribe({
+      next: (value) => {
+        this.profile = value.data;
+        this.loading = false;
+      },
+      error: (error) => {
+        console.log(error);
+        this.loading = false;
+        this.profile = null;
+      },
+    });
   }
 }
