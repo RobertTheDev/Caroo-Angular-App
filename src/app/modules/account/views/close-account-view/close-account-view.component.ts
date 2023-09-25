fa45b9a567aeab394a9a0db8715251b64609bdbc
RobertTheDev <robertthedev@gmail.com>
2023-09-25 03:47:33 +0100

    feat: created close account view

---
+++ src/app/modules/account/views/close-account-view/close-account-view.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-close-account-view',
+  templateUrl: './close-account-view.component.html',
+  styleUrls: ['./close-account-view.component.css']
+})
+export class CloseAccountViewComponent {
+
+}
