f13a08af250c01accd0499371dc25ed0270f516f
RobertTheDev <robertthedev@gmail.com>
2023-09-25 02:47:36 +0100

    feat: created seller view component

---
+++ src/app/modules/seller/views/seller-view/seller-view.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-seller-view',
+  templateUrl: './seller-view.component.html',
+  styleUrls: ['./seller-view.component.css'],
+})
+export class SellerViewComponent {}
