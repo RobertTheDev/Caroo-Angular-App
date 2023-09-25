c91b5663a31970190207e5aaf41ccec6bea6dfec
RobertTheDev <robertthedev@gmail.com>
2023-09-25 07:58:52 +0100

    feat: passed car view data to car detail

--- src/app/modules/car/components/car-detail/car-detail.component.ts
+++ src/app/modules/car/components/car-detail/car-detail.component.ts
@@ -1,7 +1,4 @@
-import { Component, OnInit } from '@angular/core';
-import { Meta, Title } from '@angular/platform-browser';
-import { ActivatedRoute } from '@angular/router';
-import { CarService } from 'src/app/services/car/car.service';
+import { Component, Input } from '@angular/core';
 import { formatDistanceToNow } from 'date-fns';
 import priceUnits from 'src/app/lib/units/priceUnits';
 import { faHeart, faMessage } from '@fortawesome/free-regular-svg-icons';
@@ -12,8 +9,8 @@ import ICar from 'models/car/types/Car';
   templateUrl: './car-detail.component.html',
   styleUrls: ['./car-detail.component.css'],
 })
-export class CarDetailComponent implements OnInit {
-  car: ICar | null = null;
+export class CarDetailComponent {
+  @Input() car: ICar | undefined = undefined;
   loading = true;
   errorMessage: string | null = null;
   priceUnits = priceUnits;
@@ -21,46 +18,7 @@ export class CarDetailComponent implements OnInit {
   faHeart = faHeart;
   faMessage = faMessage;
 
-  constructor(
-    private meta: Meta,
-    private title: Title,
-    private route: ActivatedRoute,
-    private carService: CarService,
-  ) {}
-
   formatDistance(date: Date) {
     return formatDistanceToNow(date, { addSuffix: true });
-  }
-
-  ngOnInit() {
-    const id = this.route.snapshot.params['id'];
-
-    this.carService.getCarById(id).subscribe({
-      next: (data) => {
-        this.loading = false;
-        this.car = data.data;
-
-        if (this.car) {
-          const { year, make, model } = this.car;
-
-          this.title.setTitle(`${year} ${make} ${model} | Caroo`);
-
-          this.meta.updateTag({
-            name: 'description',
-            content: `Buy a used ${make} ${model} from caroo. Explore a wide selection of high-quality, second-hand used cars for sale in your local area at Caroo. Find the perfect vehicle to meet your needs and budget. Start your car search with confidence at Caroo.`,
-          });
-        } else {
-          this.title.setTitle(`Car | Caroo`);
-          this.meta.updateTag({
-            name: 'description',
-            content: `Explore a wide selection of high-quality, second-hand used cars for sale in your local area at Caroo. Find the perfect vehicle to meet your needs and budget. Start your car search with confidence at Caroo.`,
-          });
-        }
-      },
-      error: (error) => {
-        this.loading = false;
-        this.errorMessage = error;
-      },
-    });
   }
 }
--- src/app/modules/car/views/car-view/car-view.component.ts
+++ src/app/modules/car/views/car-view/car-view.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import ICar from 'models/car/types/Car';
+import { CarService } from 'src/app/services/car/car.service';
 import { MetaService } from 'src/app/services/meta/meta.service';
 
 @Component({
@@ -6,9 +9,55 @@ import { MetaService } from 'src/app/services/meta/meta.service';
   templateUrl: './car-view.component.html',
 })
 export class CarViewComponent implements OnInit {
-  constructor(private metaService: MetaService) {}
+  constructor(
+    private carService: CarService,
+    private metaService: MetaService,
+    private route: ActivatedRoute,
+  ) {}
+
+  car: ICar | null = null;
 
   ngOnInit() {
-    this.metaService.setMeta('Title', 'Description.', 'Keywords', 'Path');
+    const id = this.route.snapshot.params['id'];
+
+    this.carService.getCarById(id).subscribe({
+      next: (data) => {
+        this.car = data.data;
+      },
+      error: (error) => {
+        console.log(error.error);
+      },
+    });
   }
+  //   const id = this.route.snapshot.params['id'];
+
+  //   this.metaService.setMeta('Title', 'Description.', 'Keywords', 'Path');
+
+  //   this.carService.getCarById(id).subscribe({
+  //     next: (data) => {
+  //       this.loading = false;
+  //       this.car = data.data;
+
+  //       if (this.car) {
+  //         const { year, make, model } = this.car;
+
+  //         this.title.setTitle(`${year} ${make} ${model} | Caroo`);
+
+  //         this.meta.updateTag({
+  //           name: 'description',
+  //           content: `Buy a used ${make} ${model} from caroo. Explore a wide selection of high-quality, second-hand used cars for sale in your local area at Caroo. Find the perfect vehicle to meet your needs and budget. Start your car search with confidence at Caroo.`,
+  //         });
+  //       } else {
+  //         this.title.setTitle(`Car | Caroo`);
+  //         this.meta.updateTag({
+  //           name: 'description',
+  //           content: `Explore a wide selection of high-quality, second-hand used cars for sale in your local area at Caroo. Find the perfect vehicle to meet your needs and budget. Start your car search with confidence at Caroo.`,
+  //         });
+  //       }
+  //     },
+  //     error: (error) => {
+  //       this.loading = false;
+  //       this.errorMessage = error;
+  //     },
+  //   });
 }
