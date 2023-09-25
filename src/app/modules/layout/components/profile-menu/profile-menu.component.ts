a7c06af8c35c5314ed14e7bebdf880d6d1ec141a
RobertTheDev <robertthedev@gmail.com>
2023-09-25 03:56:56 +0100

    feat: added layout service

--- src/app/modules/layout/components/profile-menu/profile-menu.component.ts
+++ src/app/modules/layout/components/profile-menu/profile-menu.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
+import profileMenuLinks from 'src/app/lib/links/profileMenuLinks';
+import { LayoutService } from 'src/app/services/layout/layout.service';
 
 @Component({
   selector: 'app-profile-menu',
   templateUrl: './profile-menu.component.html',
-  styleUrls: ['./profile-menu.component.css']
+  styleUrls: ['./profile-menu.component.css'],
 })
 export class ProfileMenuComponent {
+  constructor(private layoutService: LayoutService) {}
 
+  profileMenuLinks = profileMenuLinks;
+
+  get profileMenuActive(): boolean {
+    return this.layoutService.profileMenuActive;
+  }
+
+  toggleProfileMenu() {
+    this.layoutService.toggleProfileMenu();
+  }
 }
