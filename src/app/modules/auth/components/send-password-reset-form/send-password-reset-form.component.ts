2fd82b22420dfff9b3ad8f1c2627c652f4b2b2b3
RobertTheDev <robertthedev@gmail.com>
2023-09-25 11:02:33 +0100

    feat: added form functionality

--- src/app/modules/auth/components/send-password-reset-form/send-password-reset-form.component.ts
+++ src/app/modules/auth/components/send-password-reset-form/send-password-reset-form.component.ts
@@ -1,7 +1,5 @@
 import { Component } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
-import { Subscription } from 'rxjs';
-import { AuthService } from 'src/app/services/auth/auth.service';
 
 @Component({
   selector: 'app-send-password-reset-form',
@@ -11,8 +9,6 @@ export class SendPasswordResetFormComponent {
   constructor(
     // Angular form builder is used to build forms in Angular with less code.
     private formBuilder: FormBuilder,
-    // Calls our auth service to get access to our auth function helpers.
-    private authService: AuthService,
   ) {}
 
   // Variables are used for handling form states - submitted, loading and errors.
@@ -27,20 +23,19 @@ export class SendPasswordResetFormComponent {
       [
         // Email address cannnot be empty and must be in valid email format.
         Validators.required,
-        Validators.email,
       ],
     ],
   });
 
-  // Get access to the the form controls to be used in validation messaging in HTML.
+  // Get access to the the form controls to be used in validation messagin in HTML.
   get formControls() {
     return this.sendPasswordResetForm.controls;
   }
 
-  // Send password reset function calls the send password reset token handler from our auth service to handle sending password reset token.
+  // Login function calls the sign up handler from our auth service to handle user login.
   // The function handles validation and wont submit until fields are valid.
   // The function handles errors and displays error response messages.
-  handleSendPasswordReset(): Subscription | undefined {
+  handleSendPasswordReset() {
     // When function is called set form submitted to true and error message to null.
     this.formSubmitted = true;
     this.formErrorMessage = null;
@@ -49,23 +44,9 @@ export class SendPasswordResetFormComponent {
     if (this.sendPasswordResetForm.invalid) {
       return;
     }
-
-    // Get the value data the form.
+    // Get the value data from the login form.
     const { value } = this.sendPasswordResetForm;
 
-    // Start loading while form is being processed.
-    this.formLoading = true;
-
-    return this.authService.sendPasswordResetToken(value).subscribe({
-      // If form has successfully handled sending password reset - stop the form loading.
-      next: () => {
-        this.formLoading = false;
-      },
-      // If an error occurs then contain the error message in the variable. Stop form loading.
-      error: (error) => {
-        this.formLoading = false;
-        this.formErrorMessage = error.error.statusMessage;
-      },
-    });
+    alert(JSON.stringify(value));
   }
 }
