8d08cfbced53b8b4274d809c48b5eb4b98f475b3
RobertTheDev <robertthedev@gmail.com>
2023-09-25 06:48:07 +0100

    feat: added meta service

--- src/app/modules/auth/views/sign-up-view/sign-up-view.component.ts
+++ src/app/modules/auth/views/sign-up-view/sign-up-view.component.ts
@@ -1,22 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { Meta, Title } from '@angular/platform-browser';
+import { MetaService } from 'src/app/services/meta/meta.service';
 
 @Component({
   selector: 'app-sign-up-view',
   templateUrl: './sign-up-view.component.html',
 })
 export class SignUpViewComponent implements OnInit {
-  constructor(
-    private meta: Meta,
-    private title: Title,
-  ) {}
-  ngOnInit(): void {
-    this.title.setTitle('Sign Up | Caroo');
-
-    this.meta.updateTag({
-      name: 'description',
-      content:
-        'Enter your email, name and password to sign up for an account on Caroo. Your password must be at least eight characters long with at least one number, capital letter and special character.',
-    });
-  }
+  constructor(metaService: MetaService) {}
 }
