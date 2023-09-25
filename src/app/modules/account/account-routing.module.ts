0c184f26b4a63185b09a8c62f56281df7aabc826
RobertTheDev <robertthedev@gmail.com>
2023-09-25 04:09:39 +0100

    feat: updated account routing module routes

--- src/app/modules/account/account-routing.module.ts
+++ src/app/modules/account/account-routing.module.ts
@@ -4,22 +4,12 @@ import { UpdateEmailViewComponent } from './views/update-email-view/update-email
 import { UpdatePasswordViewComponent } from './views/update-password-view/update-password-view.component';
 import { VerifyEmailWithTokenViewComponent } from './views/verify-email-with-token-view/verify-email-with-token-view.component';
 import { SendEmailVerificationTokenViewComponent } from './views/send-email-verification-token-view/send-email-verification-token-view.component';
-import { AccountProfileViewComponent } from './views/account-profile-view/account-profile-view.component';
-import { UpdateAccountProfileViewComponent } from './views/update-account-profile-view/update-account-profile-view.component';
 import { CloseAccountViewComponent } from './views/close-account-view/close-account-view.component';
 
 const routes: Routes = [
   {
     path: 'close-account',
     component: CloseAccountViewComponent,
-  },
-  {
-    path: 'profile',
-    component: AccountProfileViewComponent,
-  },
-  {
-    path: 'profile/edit',
-    component: UpdateAccountProfileViewComponent,
   },
   {
     path: 'update-email',
