3c555632fa9340d354cc0aeaabcbd659c4a9974a
RobertTheDev <robertthedev@gmail.com>
2023-09-26 04:06:53 +0100

    ci: removed profile routes

--- api/router/routes/account.route.ts
+++ api/router/routes/account.route.ts
@@ -1,9 +1,7 @@
 import changeEmailAddress from 'api/controllers/account/changeEmailAddress';
 import changePassword from 'api/controllers/account/changePassword';
 import closeAccount from 'api/controllers/account/closeAccount';
-import getProfile from 'api/controllers/account/getProfile';
 import sendEmailVerificationToken from 'api/controllers/account/sendEmailVerificationToken';
-import updateProfile from 'api/controllers/account/updateProfile';
 import verifyEmailWithToken from 'api/controllers/account/verifyEmailWithToken';
 import isEmailVerificationTokenExpired from 'api/middlewares/account/isEmailVerificationTokenExpired';
 import isAuthenticated from 'api/middlewares/auth/isAuthenticated';
@@ -12,10 +10,6 @@ import { Router } from 'express';
 
 // Define account router using the express router.
 const accountRouter = Router();
-
-accountRouter.get('/get-profile', isAuthenticated, getProfile);
-
-accountRouter.put('/update-profile', isAuthenticated, updateProfile);
 
 accountRouter.put(
   '/change-email-address',
