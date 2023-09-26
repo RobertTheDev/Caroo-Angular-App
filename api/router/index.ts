3dfc741f6cbfe3ed84577230e7e4aba90d731781
RobertTheDev <robertthedev@gmail.com>
2023-09-26 04:08:21 +0100

    feat: added profile route

--- api/router/index.ts
+++ api/router/index.ts
@@ -6,6 +6,7 @@ import authRouter from './routes/auth.route';
 import accountRouter from './routes/account.route';
 import carImagesRouter from './routes/carImages.route';
 import carRequestsRouter from './routes/carRequests.route';
+import profileRouter from './routes/profile.route';
 
 // App router defines all the routes used in the application.
 // App router is exported into the main server file.
@@ -17,6 +18,7 @@ appRouter.use('/auth', authRouter);
 appRouter.use('/car-images', carImagesRouter);
 appRouter.use('/car-requests', carRequestsRouter);
 appRouter.use('/cars', carsRouter);
+appRouter.use('/profile', profileRouter);
 appRouter.use('/saved-cars', savedCarsRouter);
 appRouter.use('/users', usersRouter);
 
