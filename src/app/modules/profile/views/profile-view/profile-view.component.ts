87613e783a7c2d17fae51adca463da3415130b01
RobertTheDev <robertthedev@gmail.com>
2023-09-26 04:07:43 +0100

    feat: added profile service

--- src/app/modules/profile/views/profile-view/profile-view.component.ts
+++ src/app/modules/profile/views/profile-view/profile-view.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import IUser from 'models/user/types/User';
-import { AuthService } from 'src/app/services/auth/auth.service';
 import { MetaService } from 'src/app/services/meta/meta.service';
+import { ProfileService } from 'src/app/services/profile/profile.service';
 
 @Component({
   selector: 'app-profile-view',
@@ -10,7 +10,7 @@ import { MetaService } from 'src/app/services/meta/meta.service';
 export class ProfileViewComponent implements OnInit {
   constructor(
     private metaService: MetaService,
-    private authService: AuthService,
+    private profileService: ProfileService,
   ) {}
 
   profile: IUser | null = null;
@@ -19,7 +19,7 @@ export class ProfileViewComponent implements OnInit {
   ngOnInit() {
     this.metaService.setMeta('Profile', 'Description.', 'Keywords', 'profile');
 
-    this.authService.getAuthenticatedUser().subscribe({
+    this.profileService.getProfile().subscribe({
       next: (value) => {
         this.profile = value.data;
         this.loading = false;
