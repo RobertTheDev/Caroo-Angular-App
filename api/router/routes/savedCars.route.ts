6c99eb6cb24deddbc1c6d1092cda50ecef1b412a
RobertTheDev <robertthedev@gmail.com>
2023-09-27 14:40:22 +0100

    ci: removed deprecated routes

--- api/router/routes/savedCars.route.ts
+++ api/router/routes/savedCars.route.ts
@@ -1,10 +1,7 @@
 import createSavedCar from 'api/controllers/savedCar/createSavedCar';
 import deleteSavedCarsByUserId from 'api/controllers/savedCar/deleteSavedCarsByUserId';
-import getSavedCarById from 'api/controllers/savedCar/getSavedCarById';
-import getSavedCars from 'api/controllers/savedCar/getSavedCars';
 import getSavedCarsByUserId from 'api/controllers/savedCar/getSavedCarsByUserId';
 import isAuthenticated from 'api/middlewares/auth/isAuthenticated';
-import { deleteAllSavedCarsByCarId } from 'api/providers/prisma/savedCar.service';
 import { Router } from 'express';
 
 // Saved cars router defines all the routes used in modifying saved cars data.
@@ -13,26 +10,10 @@ import { Router } from 'express';
 // Define saved cars router using the express router.
 const savedCarsRouter = Router();
 
-savedCarsRouter.delete(
-  '/car/:carId',
-  isAuthenticated,
-  deleteAllSavedCarsByCarId,
-);
+savedCarsRouter.delete('/', isAuthenticated, deleteSavedCarsByUserId);
 
-savedCarsRouter.delete(
-  '/user/:userId',
-  isAuthenticated,
-  deleteSavedCarsByUserId,
-);
+savedCarsRouter.get('/', getSavedCarsByUserId);
 
-savedCarsRouter.get('/', getSavedCars);
-
-savedCarsRouter.get('/:id', getSavedCarById);
-
-savedCarsRouter.get('/user/:userId', getSavedCarsByUserId);
-
-savedCarsRouter.get('/:id', getSavedCarById);
-
-savedCarsRouter.post('/save-car', isAuthenticated, createSavedCar);
+savedCarsRouter.post('/save-car/:carId', isAuthenticated, createSavedCar);
 
 export default savedCarsRouter;
