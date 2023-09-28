42422df9b839f3b131cb0a63d19e58cc7281ef8e
RobertTheDev <robertthedev@gmail.com>
2023-09-28 12:16:56 +0100

    feat: added cars service

--- src/app/modules/profile/views/my-car-listings-view/my-car-listings-view.component.ts
+++ src/app/modules/profile/views/my-car-listings-view/my-car-listings-view.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import ICar from 'models/car/types/Car';
+import { CarService } from 'src/app/services/car/car.service';
 import { MetaService } from 'src/app/services/meta/meta.service';
 
 @Component({
@@ -6,7 +8,14 @@ import { MetaService } from 'src/app/services/meta/meta.service';
   templateUrl: './my-car-listings-view.component.html',
 })
 export class MyCarListingsViewComponent implements OnInit {
-  constructor(private metaService: MetaService) {}
+  cars: ICar[] = [];
+  loading = true;
+  errorMessage: string | null = null;
+
+  constructor(
+    private metaService: MetaService,
+    private carService: CarService,
+  ) {}
 
   ngOnInit() {
     this.metaService.setMeta(
@@ -15,5 +24,16 @@ export class MyCarListingsViewComponent implements OnInit {
       'Keywords',
       'my-car-listings',
     );
+
+    this.carService.getCarsByAuthenticatedUser().subscribe({
+      next: (data) => {
+        this.loading = false;
+        this.cars = data.data;
+      },
+      error: (error) => {
+        this.loading = false;
+        this.errorMessage = error;
+      },
+    });
   }
 }
