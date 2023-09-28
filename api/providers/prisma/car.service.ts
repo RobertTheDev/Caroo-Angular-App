c02a25999de91650c4451e93d69fb58dcaef9a0c
RobertTheDev <robertthedev@gmail.com>
2023-09-28 05:53:15 +0100

    ci: removed images handler from update car

--- api/providers/prisma/car.service.ts
+++ api/providers/prisma/car.service.ts
@@ -197,51 +197,11 @@ export async function updateOneCarById(
   data: UpdateCarSchemaType,
   id: string,
 ): Promise<Car> {
-  const { images, ...car } = data;
-
   // Create the car without images
-  const createdCar = await prisma.car.update({
-    data: car,
+  return await prisma.car.update({
+    data,
     where: { id },
   });
-
-  // Create car images and associate them with the car
-  const createdCarImages = await Promise.all(
-    images.map(async (imageData) => {
-      const { alt, url } = imageData;
-      return await prisma.carImage.create({
-        data: {
-          alt,
-          url,
-          car: {
-            connect: {
-              id: createdCar.id,
-            },
-          },
-        },
-      });
-    }),
-  );
-  // Attach the created images to the car
-  const updatedCar = await prisma.car.update({
-    include: {
-      owner: {
-        select: userReturnFields,
-      },
-    },
-    where: {
-      id: createdCar.id,
-    },
-    data: {
-      images: {
-        connect: createdCarImages.map((image) => ({
-          id: image.id,
-        })),
-      },
-    },
-  });
-
-  return updatedCar;
 }
 
 // Delete all cars.
